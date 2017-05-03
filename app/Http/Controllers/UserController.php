00904543e6f80100b7e1fd1b2a4021fd1ece868b
Ademola Plumptre <dplumptre@yahoo.com>
2017-05-03 19:43:14 +0100

    payment

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -44,14 +44,20 @@ class UserController extends Controller
 
     public function dashboardAdmin(User $users, Payer $payer, Pair $pair)
     {
-        $rid = auth()->user()->receivers()->select('id')->first();
+        $rid = auth()->user()->receivers()->where('status', '>', self::PENDING)->pluck('id');
+        
+        //dd($rid);
+        
         $paymenttins = 0;
 
         if (count($pair->completedRecRows())) {
             $paymenttins = $pair::where('receiver_status', self::COMPLETED)
-                ->where('receiver_id', $rid->id)
+                ->whereIn('receiver_id', $rid)
                 //->toSql();
                 ->get();
+            
+            
+           // dd($paymenttins);
 
             if (!$paymenttins) {
                 $paymenttins = 0;
